061b9aff40e6980746a379ad4b2428484d4074e2
voicedhealer <vivien.bernardot@gmail.com>
2025-06-16 10:15:03 +0200

    add class user

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,1 +1,12 @@
 from base import BaseModel
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, password):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+
+
