46a01d03e019f52ff6e310bc7c245988bffec36b
o0anais0o <choisy_anais@hotmail.fr>
2025-06-17 10:11:12 +0200

    Réorganisation et ajout de commentaires pour les méthodes de gestion des utilisateurs, des lieux, des avis et des commodités dans HBnBFacade.

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -7,43 +7,65 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Créer un utilisateur
+    # ---------- USER ----------
     def create_user(self, user_data):
-        # user_data est un dictionnaire avec les infos de l'utilisateur
         user = self.user_repo.create(user_data)
-        # On retire le mot de passe avant de retourner l'utilisateur
         if 'password' in user:
-            user = dict(user)  # Copie pour ne pas modifier l'original
+            user = dict(user)
             user.pop('password')
         return user
 
-    # Récupérer la liste de tous les utilisateurs
     def get_all_users(self):
         users = self.user_repo.get_all()
-        # On retire le mot de passe pour chaque utilisateur
-        return [
-            {k: v for k, v in user.items() if k != 'password'}
-            for user in users
-        ]
+        return [{k: v for k, v in user.items() if k != 'password'} for user in users]
 
-    # Récupérer un utilisateur par ID
     def get_user(self, user_id):
         user = self.user_repo.get(user_id)
         if user is None:
             return None
-        # On retire le mot de passe
         return {k: v for k, v in user.items() if k != 'password'}
 
-    # Mettre à jour un utilisateur
     def update_user(self, user_id, update_data):
         user = self.user_repo.update(user_id, update_data)
         if user is None:
             return None
-        # On retire le mot de passe
         return {k: v for k, v in user.items() if k != 'password'}
 
-    # Exemple pour Place (à compléter selon la logique métier)
+    # ---------- PLACE ----------
+    def create_place(self, place_data):
+        return self.place_repo.create(place_data)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
     def get_place(self, place_id):
         return self.place_repo.get(place_id)
 
-    # Ajoute ici les autres méthodes pour Place, Review, Amenity selon le même modèle
+    def update_place(self, place_id, update_data):
+        return self.place_repo.update(place_id, update_data)
+
+    # ---------- REVIEW ----------
+    def create_review(self, review_data):
+        return self.review_repo.create(review_data)
+
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def update_review(self, review_id, update_data):
+        return self.review_repo.update(review_id, update_data)
+
+    # ---------- AMENITY ----------
+    def create_amenity(self, amenity_data):
+        return self.amenity_repo.create(amenity_data)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def update_amenity(self, amenity_id, update_data):
+        return self.amenity_repo.update(amenity_id, update_data)
