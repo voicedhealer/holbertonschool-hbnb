1980c95118458955c59cffa233c37ee599984754
alizium <10549@holbertonstudents.com>
2025-06-18 11:13:53 +0200

    ...

--- part2/run.py
+++ part2/run.py
@@ -12,7 +12,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # 👇 Ouvre Swagger UI automatiquement
+    # Ouvre Swagger UI automatiquement
     webbrowser.open("http://localhost:5001/api/v1/")
     
     # Lance le serveur sur le port 5001
