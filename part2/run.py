9cce00862ecf2d43a012f3d2f93c72a04be30edb
alizium <10549@holbertonstudents.com>
2025-06-18 11:13:26 +0200

    ...

--- part2/run.py
+++ part2/run.py
@@ -1,15 +1,19 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-# ou, pour être sûr :
-# sys.path.insert(0, '/home/choisy/holbertonschool-hbnb/part2/')
+import webbrowser
 
-print("PYTHONPATH:", sys.path)  # Debug temporairefrom app import create_app
+# Chemin du projet
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+print("PYTHONPATH:", sys.path)  # Debug temporaire
 
 from app import create_app
-
 
 app = create_app()
 
 if __name__ == "__main__":
+    # 👇 Ouvre Swagger UI automatiquement
+    webbrowser.open("http://localhost:5001/api/v1/")
+    
+    # Lance le serveur sur le port 5001
     app.run(host='0.0.0.0', port=5001)
