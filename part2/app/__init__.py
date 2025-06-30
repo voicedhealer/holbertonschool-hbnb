12a8972073d7ab851dbff82f57ef86f69e96bd97
o0anais0o <choisy_anais@hotmail.fr>
2025-06-30 14:20:06 +0200

    ajout de l'initialisation de JWTManager et de la configuration de la base de données dans create_app

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,17 +1,31 @@
 from flask import Flask
 from flask_restx import Api
+from flask_jwt_extended import JWTManager
+from .models import db
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
-def create_app(config_class="config.DevelopmentConfig"):
+def create_app(config_class="app.config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # Initialisation des extensions
+    db.init_app(app)
+    jwt = JWTManager(app)
+
+    # Initialisation de l'API RESTX
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
+    # Enregistrement des namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
+    # Si tu as des blueprints classiques, tu peux les enregistrer ici
+    # from .routes import bp as main_bp
+    # app.register_blueprint(main_bp)
+
     return app
