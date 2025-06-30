51ef16dd468741810b1aa2b81c1789b21be99d63
o0anais0o <choisy_anais@hotmail.fr>
2025-06-30 14:15:55 +0200

    ajout de la configuration de SQLAlchemy dans __init__.py et définition du modèle User dans user.py

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,4 +1,5 @@
 from .basemodel import BaseModel
+from . import db
 import re
 
 class User(BaseModel):
@@ -81,3 +82,9 @@ class User(BaseModel):
             'last_name': self.last_name,
             'email': self.email
         }
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
