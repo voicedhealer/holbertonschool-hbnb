112f7651adbee1265528aa6b3c1d76a56270442d
voicedhealer <vivien.bernardot@gmail.com>
2025-06-30 14:15:12 +0200

    update init py in app

--- part2/config.py
+++ part2/config.py
@@ -2,10 +2,16 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SQLALCHEMY_DATABASE_URI = 'sqlight:///db.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'jwt-secret'
     DEBUG = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
+class ProdutionConfig(Config):
+    DEBUG = False
 
 config = {
     'development': DevelopmentConfig,
