d35d59791b70c5f0fbd2f8e5756eb1227efdcc70
voicedhealer <vivien.bernardot@gmail.com>
2025-07-02 14:56:52 +0200

    update persistence

--- hbnb/part3/app/services/facade.py
+++ hbnb/part3/app/services/facade.py
@@ -40,7 +40,7 @@ class Place(BaseModel):
         self.amenities = amenities if amenities is not None else []
 
         # Convertir owner_id → objet owner (User)
-        repo = UserRepository()
+        repo = Repository()
         user_obj = repo.get(owner_id)
         if user_obj is None:
             raise ValueError(f"Aucun utilisateur trouvé avec l'ID {owner_id}")
@@ -77,10 +77,10 @@ from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = SQLAlchemyRepository(User)
-        self.amenity_repo = SQLAlchemyRepository(Amenity)
-        self.place_repo = SQLAlchemyRepository(Place)
-        self.review_repo = SQLAlchemyRepository(Review)
+        self.user_repo = InMemoryRepository(User)
+        self.amenity_repo = InMemoryRepository(Amenity)
+        self.place_repo = InMemoryRepository(Place)
+        self.review_repo = InMemoryRepository(Review)
 
     def create_user(self, user_data):
         password = user_data.pop('password')
@@ -190,4 +190,3 @@ class HBnBFacade:
         user.delete_review(review)
         place.delete_review(review)
         self.review_repo.delete(review_id)
-
